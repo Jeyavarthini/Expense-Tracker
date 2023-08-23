16c1246e4cc681a0cd6d2eb0ff5539d4f163b85a
Jeyavarthini <jeyavarthiniravi@gmail.com>
2023-08-23 13:38:51 +0530

    commit

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const cors=require('cors')
+const app=express()
+require('dotenv').config()
+
+const PORT =process.env.PORT
+app.use(express.json())
+app.use(cors())
+
+const server=()=>{
+    db()
+    app.listen(PORT,()=>{
+        console.log('listening to port:',PORT)
+    })
+}
+server()
