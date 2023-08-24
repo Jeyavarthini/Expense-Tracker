626f7d82950b010ec8f657af009b32f2c0440a92
Jeyavarthini <jeyavarthiniravi>
2023-08-24 14:14:05 +0530

    commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,25 @@
-const express=require('express')
-const cors=require('cors')
-const { db } = require('./db/db')
-const {readdirSync}=require('fs')
-const app=express()
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
+
 require('dotenv').config()
 
-const PORT =process.env.PORT
+const PORT = process.env.PORT
+
+//middlewares
 app.use(express.json())
 app.use(cors())
 
-readdirSync('./route').map((routes)=> app.use('/api/v1',require('./route/'+routes)))
-const server=()=>{
-    app.listen(PORT,()=>{
-        db()
-        console.log('listening to port:',PORT)
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
     })
 }
+
 server()
--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,12 +1,13 @@
-const mongoose=require('mongoose');
-const db = async ()=>{
+const mongoose = require('mongoose');
+
+const db = async () => {
     try {
-        mongoose.set('strictQuery',false)
+        mongoose.set('strictQuery', false)
         await mongoose.connect(process.env.MONGO_URL)
-        console.log('DB connected')
+        console.log('Db Connected')
     } catch (error) {
-        console.log('DB connection error')
+        console.log('DB Connection Error');
     }
-    
 }
-module.exports={db}
+
+module.exports = {db}
