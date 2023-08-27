86ea3522345eabac09f582f2c6e4bb2679bba7f9
Jeyavarthini <jeyavarthiniravi>
2023-08-27 12:04:09 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 
+import { styled } from 'styled-components';
+import bg from './img/bg.png'
+
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppStyled bg={bg} className="App">
+    </AppStyled>
   );
 }
+
+const AppStyled =styled.div`
+  height: 100vh;
+  background-image: url(${props =>props.bg});
+`;
 
 export default App;
