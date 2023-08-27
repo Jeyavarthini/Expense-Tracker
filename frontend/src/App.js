e3576c9a6f3439189a607f04979f6040e8b48f01
Jeyavarthini <jeyavarthiniravi>
2023-08-27 16:32:48 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
-
-import styled from "styled-components";
+import { styled } from "styled-components";
 import bg from './img/bg.png'
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
 
 
 function App() {
   return (
     <AppStyled bg={bg} className="App">
-
+      <MainLayout>
+        <Orb/>
+      </MainLayout>
     </AppStyled>
   );
 }
+      const AppStyled = styled.div`
+      height: 100vh;
+      background-image: url(${bg})`;
 
-const AppStyled = styled.div`
-  height: 100vh;
-  background-image: url(${props => props.bg});
-`;
 
 export default App;
 
