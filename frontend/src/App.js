e2bf19612e6fd0a176cd50b105568e0b817fb9e1
Jeyavarthini <jeyavarthiniravi>
2023-08-27 13:01:01 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,28 +19,3 @@ const AppStyled = styled.div`
 export default App;
 
 
-
-// import styled from "styled-components";
-// import bg from './img/bg.png';
-
-// function App() {
-//   return (
-//     // <AppStyled bg={bg} className="App">
-//     //   {/* Your app content */}
-//     // </AppStyled>
-//     <div className="App">
-//       <div style={{
-//          backgroundImage:`url(${bg})`,
-//          backgroundSize: cover
-//       }}></div>
-//     </div>
-//   );
-// }
-
-// // const AppStyled = styled.div`
-// //   height: 100vh;
-// //   backgroundImage: url(${props => props.bg});
-// //   background-size: cover; /* Adjust this as needed */
-// // `;
-
-
